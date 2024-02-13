c0b24834f81e00cd7b1a481f4d8c44bff40522a0
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-13 12:52:07 +0600

    News provider completed

--- src/providers/NewsProvider.jsx
+++ src/providers/NewsProvider.jsx
@@ -2,8 +2,10 @@ import { NewsContext } from "../context/all-contexts";
 import useNewsQuery from "../hooks/useNewsQuery";
 
 const NewsProvider = ({ children }) => {
+  // Destructuring news, isLoading, and error from the custom hook
   const { news, isLoading, error } = useNewsQuery();
 
+  // Rendering NewsContext.Provider to provide news-related data to its children
   return (
     <>
       <NewsContext.Provider value={{ news, isLoading, error }}>
