cb2de3de25421b1766d5f8ee0d5b10f866c2c0e6
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-13 13:44:32 +0600

    added categories

--- src/components/global-components/navigation-bar/Categories.jsx
+++ src/components/global-components/navigation-bar/Categories.jsx
@@ -1,29 +1,30 @@
 const Categories = () => {
+  const newsCategories = [
+    { id: crypto.randomUUID(), name: "General" },
+    { id: crypto.randomUUID(), name: "Business" },
+    { id: crypto.randomUUID(), name: "Entertainment" },
+    { id: crypto.randomUUID(), name: "Health" },
+    { id: crypto.randomUUID(), name: "Science" },
+    { id: crypto.randomUUID(), name: "Sports" },
+    { id: crypto.randomUUID(), name: "Technology" },
+    { id: crypto.randomUUID(), name: "Environment" },
+    { id: crypto.randomUUID(), name: "Education" },
+    { id: crypto.randomUUID(), name: "Fashion" },
+    { id: crypto.randomUUID(), name: "Food" },
+    { id: crypto.randomUUID(), name: "Travel" },
+    { id: crypto.randomUUID(), name: "Art & Culture" },
+    { id: crypto.randomUUID(), name: "Opinion" },
+  ];
+
   return (
     <>
       <div className="container mx-auto mt-6">
         <ul className="flex flex-wrap items-center justify-center gap-5 text-xs font-semibold lg:text-base">
-          <li>
-            <a href="#">General</a>
-          </li>
-          <li>
-            <a href="#">Business</a>
-          </li>
-          <li>
-            <a href="#">Entertainment</a>
-          </li>
-          <li>
-            <a href="#">Health</a>
-          </li>
-          <li>
-            <a href="#">Science</a>
-          </li>
-          <li>
-            <a href="#">Sports</a>
-          </li>
-          <li>
-            <a href="#">Technology</a>
-          </li>
+          {newsCategories.map((item) => (
+            <li key={item.id}>
+              <a href="#">{item.name}</a>
+            </li>
+          ))}
         </ul>
       </div>
     </>
