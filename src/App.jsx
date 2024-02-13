bc4464a082e85169f34835b2adc4661e075b7c0f
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-13 11:30:00 +0600

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./components/global-components/footer/Footer";
 import NavigationBar from "./components/global-components/navigation-bar/NavigationBar";
+import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
     <>
       <NavigationBar />
+      <LandingPage />
+      <Footer />
     </>
   );
 };
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,5 @@
+const LandingPage = () => {
+  return <>Hello World</>;
+};
+
+export default LandingPage;
