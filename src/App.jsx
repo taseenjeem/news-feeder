ac2642aac98636874fe6d770a558be9b807d5b95
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-13 13:04:05 +0600

    Wrapper the app component with news provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Footer from "./components/global-components/footer/Footer";
 import NavigationBar from "./components/global-components/navigation-bar/NavigationBar";
 import LandingPage from "./pages/LandingPage";
+import NewsProvider from "./providers/NewsProvider";
 
 const App = () => {
   return (
     <>
-      <NavigationBar />
-      <LandingPage />
-      <Footer />
+      <NewsProvider>
+        <NavigationBar />
+        <LandingPage />
+        <Footer />
+      </NewsProvider>
     </>
   );
 };
