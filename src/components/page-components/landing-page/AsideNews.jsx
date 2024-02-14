c0a8bfd6e3061542d03b8c03695e4c0ab61b250c
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-15 01:47:21 +0600

    aside news completed

--- src/components/page-components/landing-page/AsideNews.jsx
+++ src/components/page-components/landing-page/AsideNews.jsx
@@ -1,95 +1,61 @@
-import thumb2 from "../../../assets/thumb.png";
+import thumb from "../../../assets/thumb.png";
 
-const AsideNews = () => {
+const AsideNews = ({ newsWithoutDescription }) => {
+  // Find the first item with urlToImage
+  const itemWithImage = newsWithoutDescription.find((item) => item.urlToImage);
+
+  // Separate items with and without image
+  const itemsWithoutImage = newsWithoutDescription.filter(
+    (item) => !item.urlToImage
+  );
+
+  // Function to format the date
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
+
   return (
     <>
       {/* right */}
       <div className="col-span-12 self-start xl:col-span-4">
-        <div className="space-y-6 divide-y-2 divide-[#D5D1C9]">
+        <div className="space-y-6">
           {/* news item */}
           <div className="col-span-12 mb-6 md:col-span-8">
-            <img className="w-full" src={thumb2} alt="thumb" />
-            {/* info */}
-            <div className="col-span-12 mt-6 md:col-span-4">
-              <a href="#">
+            <img
+              className="w-full"
+              src={
+                itemWithImage?.urlToImage ? itemWithImage?.urlToImage : thumb
+              }
+              alt="thumb"
+            />
+          </div>
+          {/* info */}
+          {itemsWithoutImage?.map((item) => (
+            <div
+              key={item.title}
+              className="col-span-12 mt-6 md:col-span-4 border-b-2 border-[#D5D1C9]"
+            >
+              <a
+                href={item.urlToImage}
+                target="_blank"
+                className="cursor-pointer"
+              >
                 <h3 className="mb-2.5 text-xl font-bold lg:text-[20px]">
-                  Why is Uber selling its autonomous-vehicle division?
+                  {item.title}
                 </h3>
               </a>
-              <p className="text-base text-[#292219]">
-                Self-driving cars were meant to be its future
+              <p className="text-base text-[#292219]">{item.content}</p>
+              <p className="mt-5 text-base text-[#94908C]">
+                {formatDate(item.publishedAt)}
               </p>
-              <p className="mt-5 text-base text-[#94908C]">25 Feb 2021</p>
             </div>
-          </div>
-          {/* news item ends */}
-          {/* news item */}
-          <div className="col-span-12 md:col-span-8">
-            {/* info */}
-            <div className="col-span-12 md:col-span-4">
-              <a href="#">
-                <h3 className="mb-2.5 text-xl font-bold lg:text-[20px]">
-                  Why is Uber selling its autonomous-vehicle division?
-                </h3>
-              </a>
-              <p className="text-base text-[#292219]">
-                Self-driving cars were meant to be its future
-              </p>
-              <p className="mt-5 text-base text-[#94908C]">25 Feb 2021</p>
-            </div>
-          </div>
-          {/* news item ends */}
-          {/* news item */}
-          <div className="col-span-12 md:col-span-8">
-            {/* info */}
-            <div className="col-span-12 md:col-span-4">
-              <a href="#">
-                <h3 className="mb-2.5 text-xl font-bold lg:text-[20px]">
-                  Az Uber vesztesége meghaladta az egymilliárd dollárt
-                </h3>
-              </a>
-              <p className="text-base text-[#292219]">
-                Progress in artificial intelligence belies a lack of
-                transparency that is vital for its adoption, says Gary Marcus,
-                coauthor of “Rebooting AI”
-              </p>
-              <p className="mt-5 text-base text-[#94908C]">21 Feb 2021</p>
-            </div>
-          </div>
-          {/* news item ends */}
-          {/* news item */}
-          <div className="col-span-12 md:col-span-8">
-            {/* info */}
-            <div className="col-span-12 md:col-span-4">
-              <a href="#">
-                <h3 className="mb-2.5 text-xl font-bold lg:text-[20px]">
-                  The power of liberalism can combat oppression in all its forms
-                </h3>
-              </a>
-              <p className="text-base text-[#292219]">
-                True liberals stand for individual freedom, explains Deirdre
-                Nansen McCloskey, an iconoclastic economist
-              </p>
-              <p className="mt-5 text-base text-[#94908C]">18 Feb 2021</p>
-            </div>
-          </div>
-          {/* news item ends */}
-          {/* news item */}
-          <div className="col-span-12 md:col-span-8">
-            {/* info */}
-            <div className="col-span-12 md:col-span-4">
-              <a href="#">
-                <h3 className="mb-2.5 text-xl font-bold lg:text-[20px]">
-                  The power of liberalism can combat oppression in all its forms
-                </h3>
-              </a>
-              <p className="text-base text-[#292219]">
-                True liberals stand for individual freedom, explains Deirdre
-                Nansen McCloskey, an iconoclastic economist
-              </p>
-              <p className="mt-5 text-base text-[#94908C]">18 Feb 2021</p>
-            </div>
-          </div>
+          ))}
+
           {/* news item ends */}
         </div>
       </div>
