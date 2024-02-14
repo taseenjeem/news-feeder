4965b5a567117266a3a785952acc31a4d039b507
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-15 02:41:30 +0600

    minor changes

--- src/components/page-components/landing-page/MainNewsLayout.jsx
+++ src/components/page-components/landing-page/MainNewsLayout.jsx
@@ -19,42 +19,66 @@ const MainNewsLayout = ({ newsWithDescription }) => {
         {/* Render the first news item */}
         <div className="col-span-12 grid grid-cols-12 gap-4">
           {/* Render information about the first news item */}
-          <div className="col-span-12 lg:col-span-4">
-            <a href={firstItem?.url} target="_blank">
-              <h3 className="mb-2.5 text-2xl font-bold lg:text-[28px]">
-                {firstItem?.title}
-              </h3>
-            </a>
-            <p className="text-base text-[#5C5955]">{firstItem?.description}</p>
-            <p className="mt-5 text-base text-[#5C5955]">
-              {formatDate(firstItem?.publishedAt)}
-            </p>
-          </div>
+          {firstItem && (
+            <div className="col-span-12 lg:col-span-4">
+              <a href={firstItem?.url} target="_blank">
+                <h3 className="mb-2.5 text-2xl font-bold lg:text-[28px]">
+                  {firstItem?.title}
+                </h3>
+              </a>
+              <p className="text-base text-[#5C5955]">
+                {firstItem?.description}
+              </p>
+              <p className="mt-5 text-base text-[#5C5955]">
+                {formatDate(firstItem?.publishedAt)}
+              </p>
+            </div>
+          )}
           {/* Render the thumbnail image for the first news item */}
-          <div className="col-span-12 lg:col-span-8">
-            <img className="w-full" src={secondItem?.urlToImage} alt="thumb" />
-            <p className="mt-5 text-base text-[#5C5955]">{secondItem?.title}</p>
-          </div>
+          {secondItem && (
+            <div className="col-span-12 lg:col-span-8">
+              <img
+                className="w-full"
+                src={secondItem?.urlToImage}
+                alt="thumb"
+              />
+              <p className="mt-5 text-base text-[#5C5955]">
+                {secondItem?.title}
+              </p>
+            </div>
+          )}
         </div>
         {/* Render the second news item */}
         <div className="col-span-12 grid grid-cols-12 gap-4 lg:col-span-8">
           {/* Render information about the second news item */}
-          <div className="col-span-12 md:col-span-6">
-            <a href={thirdItem?.url} target="_blank">
-              <h3 className="mb-2.5 text-xl font-bold lg:text-2xl">
-                {thirdItem?.title}
-              </h3>
-            </a>
-            <p className="text-base text-[#292219]">{thirdItem?.description}</p>
-            <p className="mt-5 text-base text-[#5C5955]">
-              {formatDate(thirdItem?.publishedAt)}
-            </p>
-          </div>
+          {thirdItem && (
+            <div className="col-span-12 md:col-span-6">
+              <a href={thirdItem?.url} target="_blank">
+                <h3 className="mb-2.5 text-xl font-bold lg:text-2xl">
+                  {thirdItem?.title}
+                </h3>
+              </a>
+              <p className="text-base text-[#292219]">
+                {thirdItem?.description}
+              </p>
+              <p className="mt-5 text-base text-[#5C5955]">
+                {formatDate(thirdItem?.publishedAt)}
+              </p>
+            </div>
+          )}
           {/* Render the thumbnail image for the second news item */}
-          <div className="col-span-12 md:col-span-6">
-            <img className="w-full" src={fourthItem?.urlToImage} alt="thumb" />
-            <p className="mt-5 text-base text-[#5C5955]">{fourthItem?.title}</p>
-          </div>
+          {fourthItem && (
+            <div className="col-span-12 md:col-span-6">
+              <img
+                className="w-full"
+                src={fourthItem?.urlToImage}
+                alt="thumb"
+              />
+              <p className="mt-5 text-base text-[#5C5955]">
+                {fourthItem?.title}
+              </p>
+            </div>
+          )}
         </div>
         {/* Render the remaining news items */}
         {remainingItems.map((item) => (
