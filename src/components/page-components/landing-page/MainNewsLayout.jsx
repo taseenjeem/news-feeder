658e636158b6c3b46cf2a00cf66d23b6765fe45b
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-15 01:06:10 +0600

    added optional chaining

--- src/components/page-components/landing-page/MainNewsLayout.jsx
+++ src/components/page-components/landing-page/MainNewsLayout.jsx
@@ -8,8 +8,8 @@ const MainNewsLayout = ({ newsWithDescription }) => {
         {/* news item */}
         <div className="col-span-12 grid grid-cols-12 gap-4">
           {/* info */}
-          <div key={crypto.randomUUID()} className="col-span-12 lg:col-span-4">
-            <a href={firstItem.url} target="_blank">
+          <div className="col-span-12 lg:col-span-4">
+            <a href={firstItem?.url} target="_blank">
               <h3 className="mb-2.5 text-2xl font-bold lg:text-[28px]">
                 {firstItem?.title}
               </h3>
@@ -18,20 +18,17 @@ const MainNewsLayout = ({ newsWithDescription }) => {
             <p className="mt-5 text-base text-[#5C5955]">1 hour ago</p>
           </div>
           {/* thumb */}
-          <div key={crypto.randomUUID()} className="col-span-12 lg:col-span-8">
+          <div className="col-span-12 lg:col-span-8">
             <img className="w-full" src={secondItem?.urlToImage} alt="thumb" />
-            <p className="mt-5 text-base text-[#5C5955]">{secondItem.title}</p>
+            <p className="mt-5 text-base text-[#5C5955]">{secondItem?.title}</p>
           </div>
         </div>
         {/* news item ends */}
         {/* news item */}
-        <div
-          key={crypto.randomUUID()}
-          className="col-span-12 grid grid-cols-12 gap-4 lg:col-span-8"
-        >
+        <div className="col-span-12 grid grid-cols-12 gap-4 lg:col-span-8">
           {/* info */}
           <div className="col-span-12 md:col-span-6">
-            <a href={thirdItem.url} target="_blank">
+            <a href={thirdItem?.url} target="_blank">
               <h3 className="mb-2.5 text-xl font-bold lg:text-2xl">
                 {thirdItem?.title}
               </h3>
@@ -40,21 +37,21 @@ const MainNewsLayout = ({ newsWithDescription }) => {
             <p className="mt-5 text-base text-[#5C5955]">1 hour ago</p>
           </div>
           {/* thumb */}
-          <div key={crypto.randomUUID()} className="col-span-12 md:col-span-6">
+          <div className="col-span-12 md:col-span-6">
             <img className="w-full" src={fourthItem?.urlToImage} alt="thumb" />
-            <p className="mt-5 text-base text-[#5C5955]">{fourthItem.title}</p>
+            <p className="mt-5 text-base text-[#5C5955]">{fourthItem?.title}</p>
           </div>
         </div>
         {/* news item ends */}
         {/* news item */}
         {remainingItems.map((item) => (
           <div
-            key={crypto.randomUUID()}
+            key={item?.title}
             className="col-span-12 md:col-span-6 lg:col-span-4"
           >
             {/* info */}
             <div className="col-span-12 md:col-span-4">
-              <a href={item.url} target="_blank">
+              <a href={item?.url} target="_blank">
                 <h3 className="mb-2.5 text-xl font-bold lg:text-2xl">
                   {item?.title}
                 </h3>
