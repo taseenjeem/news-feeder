b4b6490b11fea7e1f76bd653822b5e854f94d3e4
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-15 12:10:03 +0600

    added error message in aside news

--- src/components/page-components/landing-page/AsideNews.jsx
+++ src/components/page-components/landing-page/AsideNews.jsx
@@ -34,23 +34,35 @@ const AsideNews = ({ newsWithoutDescription }) => {
               alt="thumb"
             />
           </div>
+          <h2 className="text-4xl font-bold underline underline-offset-4">
+            Related News
+          </h2>
           {/* info */}
-          {itemsWithoutImage?.map((item) => (
-            <div
-              key={item.title}
-              className="col-span-12 mt-6 md:col-span-4 border-b-2 border-[#D5D1C9]"
-            >
-              <a href={item.url} target="_blank" className="cursor-pointer">
-                <h3 className="mb-2.5 text-xl font-bold lg:text-[20px]">
-                  {item.title}
-                </h3>
-              </a>
-              <p className="text-base text-[#292219]">{item.content}</p>
-              <p className="mt-5 text-base text-[#94908C]">
-                {formatDate(item.publishedAt)}
-              </p>
+          {newsWithoutDescription?.length <= 0 ? (
+            <div className="h-40 text-center flex justify-center items-center text-lg font-semibold text-red-500">
+              Sorry! Related news for this <br /> category is not available
+              right now.
             </div>
-          ))}
+          ) : (
+            <>
+              {itemsWithoutImage?.map((item) => (
+                <div
+                  key={item.title}
+                  className="col-span-12 mt-6 md:col-span-4 border-b-2 border-[#D5D1C9]"
+                >
+                  <a href={item.url} target="_blank" className="cursor-pointer">
+                    <h3 className="mb-2.5 text-xl font-bold lg:text-[20px]">
+                      {item.title}
+                    </h3>
+                  </a>
+                  <p className="text-base text-[#292219]">{item.content}</p>
+                  <p className="mt-5 text-base text-[#94908C]">
+                    {formatDate(item.publishedAt)}
+                  </p>
+                </div>
+              ))}
+            </>
+          )}
 
           {/* news item ends */}
         </div>
